d0dbf49b25d348f33dbcac7ce4239b8bd97d7bc1
matt-vivobook <matt.kozu@gmail.com>
2025-08-10 23:13:10 -0700

    Changes extract audio to auto parse playlists to just the song

--- Youtube_vids/Extract_audio.sh
+++ Youtube_vids/Extract_audio.sh
@@ -7,8 +7,10 @@
 
 # Code:
 for arg in "$@"; do
+	# original command:
 	# yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 "$arg"
-	yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --ffmpeg-location "$(which ffmpeg)" "$arg"
+	trimmed_url="${arg%%&list*}"
+	yt-dlp -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 "$trimmed_url"
 done
 
 
